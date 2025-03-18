3e95121c32121d38483b65acc70153f59189f827
Himaryy <122810667+Himaryy@users.noreply.github.com>
2025-03-18 13:21:09 +0700

    handle and upload image using Image Kit.IO, add toaster using sonner from shadcn

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,7 +1,111 @@
-import React from "react";
+"use client";
 
-const ImageUpload = () => {
-  return <div>ImageUpload</div>;
+import config from "@/lib/config";
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
+
+const {
+  env: {
+    imageKit: { publicKey, privateKey, urlEndpoint },
+  },
+} = config;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+    const data = await response.json();
+
+    const { signature, expire, token } = data;
+
+    return { signature, expire, token };
+  } catch (error) {
+    throw new Error(`Authentication request failed: ${error}`);
+  }
+};
+
+const ImageUpload = ({
+  onFileChange,
+}: {
+  onFileChange: (filePath: string) => void;
+}) => {
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string } | null>(null);
+
+  const onError = (error: any) => {
+    console.log(error);
+
+    toast.error("Image Upload Failed", {
+      description: `Your image could not be uploaded. Please try again.`,
+      richColors: true,
+    });
+  };
+  const onSuccess = (res: any) => {
+    setFile(res);
+    // const uploadImageFile = `${urlEndpoint}${res.filePath}`;
+    onFileChange(res.filePath);
+
+    toast.success("Image Upload Successfully", {
+      description: `${res.filePath} Upload successfully`,
+      richColors: true,
+    });
+  };
+
+  return (
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        className="hidden"
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+      />
+
+      <button
+        className="upload-btn"
+        onClick={(e) => {
+          e.preventDefault();
+
+          if (ikUploadRef.current) {
+            // @ts-ignore
+            ikUploadRef.current?.click();
+          }
+        }}
+      >
+        <Image
+          src="/icons/upload.svg"
+          alt="upload-icon"
+          width={20}
+          height={20}
+          className="object-contain"
+        />
+
+        <p className="text-base text-light-100">Upload a File</p>
+
+        {file && <p className="upload-filename">{file.filePath}</p>}
+      </button>
+      {file && (
+        <IKImage
+          alt={file.filePath}
+          path={file.filePath}
+          width={500}
+          height={300}
+        />
+      )}
+    </ImageKitProvider>
+  );
 };
 
 export default ImageUpload;
--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
+      // {
+      //   protocol: "https",
+      //   hostname: "placehold.co",
+      // },
       {
         protocol: "https",
         hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
       },
     ],
   },
